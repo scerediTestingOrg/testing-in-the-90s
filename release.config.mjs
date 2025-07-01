14e0f7ad699e2475fc333ba330cc14edc6cf4e3d
Simone Ceredi <57034077+sceredi@users.noreply.github.com>
2025-07-01 18:27:44 +0200

    ci: update configuration to work correctly in scrum (#3)
    * chore(ci): only release on release branches, add multiple java versions check
    
    * ci: update release branches name
    
    * ci: always run ci jobs

--- release.config.mjs
+++ release.config.mjs
@@ -7,7 +7,7 @@ git tag -a -f v\${nextRelease.version} v\${nextRelease.version} -F CHANGELOG.md 
 export CI_COMMIT_TAG="true"
 `;
 
-const releaseBranches = ["master"];
+const releaseBranches = [{ name: "releases/**" }];
 config.branches = releaseBranches;
 
 config.plugins.push(
