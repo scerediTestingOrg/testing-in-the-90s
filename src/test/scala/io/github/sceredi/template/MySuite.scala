8c7106c3db4930233e1b804fec5a6206c078026b
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-02 17:14:30 +0200

    test: add a test

--- src/test/scala/io/github/sceredi/template/MySuite.scala
+++ src/test/scala/io/github/sceredi/template/MySuite.scala
@@ -8,3 +8,7 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers:
   "Integer" should "equal itself" in:
     val i = 2
     i should be(2)
+
+  "String" should "equal itself" in:
+    val s = "hello"
+    s should be("hello")
