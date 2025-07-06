05708d88da91ed52f47dda2b42cf6255eb9ef260
sceredi <ceredi.simone.iti@gmail.com>
2025-07-06 12:00:10 +0200

    test: add a new test

--- src/test/scala/io/github/sceredi/template/MySuite.scala
+++ src/test/scala/io/github/sceredi/template/MySuite.scala
@@ -12,3 +12,6 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers:
   "String" should "equal itself" in:
     val s = "hello"
     s should be("hello")
+
+  "msg size" should "say something nice" in:
+    msg should be("I was compiled by Scala 3. :)")
