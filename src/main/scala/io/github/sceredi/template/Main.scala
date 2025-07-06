3df1a6c14bcf4cf869dd7b37be550f803947a59f
sceredi <ceredi.simone.iti@gmail.com>
2025-07-06 12:17:27 +0200

    chore: doing stuff

--- src/main/scala/io/github/sceredi/template/Main.scala
+++ src/main/scala/io/github/sceredi/template/Main.scala
@@ -3,5 +3,3 @@
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
-
-val imanint: Int = 1
--- src/test/scala/io/github/sceredi/template/MySuite.scala
+++ src/test/scala/io/github/sceredi/template/MySuite.scala
@@ -5,16 +5,9 @@ import matchers.*
 
 class ExampleSpec extends AnyFlatSpec with should.Matchers:
 
-  "Integer" should "equal itself" in:
-    val i = 2
-    i should be(2)
-
   "String" should "equal itself" in:
     val s = "hello"
     s should be("hello")
 
   "msg size" should "say something nice" in:
     msg should be("I was compiled by Scala 3. :)")
-
-  "anint" should "equal 1" in:
-    anint should be(1)
