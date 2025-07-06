cba411419e42888337475f7b152d2ae7fde36546
sceredi <ceredi.simone.iti@gmail.com>
2025-07-06 12:39:44 +0200

    chore: coveralls test

--- src/test/scala/io/github/sceredi/template/MySuite.scala
+++ src/test/scala/io/github/sceredi/template/MySuite.scala
@@ -5,6 +5,10 @@ import matchers.*
 
 class ExampleSpec extends AnyFlatSpec with should.Matchers:
 
+  "Integer" should "equal itself" in:
+    val i = 2
+    i should be(2)
+
   "String" should "equal itself" in:
     val s = "hello"
     s should be("hello")
